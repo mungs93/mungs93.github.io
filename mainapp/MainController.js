5965655c51cf648ba1851e87c299cfe576620ee6
mungs93 <textbookconsultants@gmail.com>
2019-06-21 02:00:30 -0500

    Save

--- mainapp/MainController.js
+++ mainapp/MainController.js
@@ -4,43 +4,37 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/obedience2.jpg', 
 	    title: 'Obedience', 
 	    developer: 'Memorial Church, Cambridge, MA.', 
-	    link: " ",
-	    price: 100
+	    link: " "
 	  }, 
 	  { 
 	    icon: 'images/pearl2.jpg', 
 	    title: 'Loyalty', 
 	    developer: 'Arch Iron Gate, New Haven, CT.', 
-	    link: " ",
-	    price: 100
+	    link: " "
 	  },
 	  {
 	    icon: 'images/disclosure.png',
 	    title: 'Disclosure',
 	    developer: 'Memorial Church, Stanford, CA.',
-	    link: " ",
-	    price: 100%
+	    link: " "
 	  },
 	  {
 	    icon: 'images/confidential.png',
 	    title: 'Confidentiality',
 	    developer: 'College Hall, Philadelphia, PA.',
-	    link: " ",
-	    price: 100%
+	    link: " "
 	  },
 	   {
 	    icon: 'images/accounting.jpg',
 	    title: 'Accounting',
 	    developer: 'College Hall, Philadelphia, PA.',
-	    link: " ",
-	    price: 100% 
+	    link: " "
 	  },
 	  {
 	    icon: 'images/reasonable.png',
 	    title: 'Reasonable Care and Diligence',
 	    developer: 'Silverman Hall, Philadelphia, PA',
-	    link: " ",
-	    price: 100%
+	    link: " "
 	  }
 	];
 }]);
