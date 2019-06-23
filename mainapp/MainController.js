47dd0ee92f07c59ac402f03f92652371afaa7ee9
mungs93 <textbookconsultants@gmail.com>
2019-06-23 01:52:41 -0500

    Save

--- mainapp/MainController.js
+++ mainapp/MainController.js
@@ -4,7 +4,7 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/obedience2.png', 
 	    title: 'Obedience', 
 	    developer: 'Memorial Church, Cambridge, MA.', 
-	    link: "images/texas.png"
+	    link: " "
 	  }, 
 	  { 
 	    icon: 'images/pearl2.jpg', 
